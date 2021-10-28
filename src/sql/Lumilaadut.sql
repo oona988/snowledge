efb588a79acc14b749bea35b81149e77b11b6d2a
abdullaharif <me@example.com>
2021-10-28 13:27:39 +0300

    Db changes in files

--- src/sql/Lumilaadut.sql
+++ src/sql/Lumilaadut.sql
@@ -1,6 +1,18 @@
-INSERT INTO Lumilaadut(Nimi, Vari)
-VALUES ('Pehmeä lumi', '#76c4d6'),
-        ('Tuulen pieksämä aaltoileva lumi', '#3f7089'),
-        ('Korppu', '#3838a0'),
-        ('Sohjo', '#7a357c'),
-        ('Jää', '#b533b2');
+-- INSERT INTO Lumilaadut(Nimi, Vari, Ennatysaika)
+-- VALUES ('Pehmeä lumi', '#76c4d6' ,null),
+--         ('Tuulen pieksämä aaltoileva lumi', '#3f7089',null),
+--         ('Korppu', '#3838a0',null),
+--         ('Sohjo', '#7a357c',null),
+--         ('Jää', '#b533b2',null);
+
+INSERT INTO Lumilaadut
+VALUES (1,'Pehmeä lumi','#76c4d6',NULL),
+        (2,'Tuulen pieksämä aaltoileva lumi','#3f7089',NULL),
+        (3,'Korppu','#3838a0',NULL),
+        (4,'Sohjo','#7a357c',NULL),
+        (5,'Jää','#b533b2',NULL),
+        (6,'Uusi Lumi','',NULL),
+        (7,'Tuulen pieksemä lumi','',NULL),
+        (8,'Vähäinen lumi','',NULL),
+        (10,'Haitat','',NULL),
+        (11,'Lumeton maa',NULL,NULL);
--- src/sql/luonnit.sql
+++ src/sql/luonnit.sql
@@ -20,7 +20,8 @@ CREATE TABLE Kayttajat (
 CREATE TABLE Lumilaadut (
     ID SERIAL PRIMARY KEY,
     Nimi VARCHAR(50),
-    Vari VARCHAR(15)
+    Vari VARCHAR(15),
+    Ennatysaika datetime DEFAULT NULL
 );
 
 CREATE TABLE Paivitykset (
@@ -29,8 +30,12 @@ CREATE TABLE Paivitykset (
     Aika DATETIME,
     Lumilaatu INT,
     Teksti TEXT,
+    Kuvaus TEXT,
+    lumen_kuva BLOB,
+    Lumilaadut_ID bigint(20) unsigned,
     FOREIGN KEY(Tekija) references Kayttajat(ID) ON DELETE CASCADE,
     FOREIGN KEY(Segmentti) references Segmentit(ID) ON DELETE CASCADE,
+    CONSTRAINT Lumilaadut_ID FOREIGN KEY (Lumilaadut_ID) REFERENCES Lumilaadut (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
     CONSTRAINT tunniste PRIMARY KEY (Aika, Segmentti)
 );
 
@@ -40,4 +45,15 @@ CREATE TABLE Koordinaatit(
     Sijainti Point,
     FOREIGN KEY(Segmentti) references Segmentit(ID) ON DELETE CASCADE,
     CONSTRAINT tunniste PRIMARY KEY(Jarjestys, Segmentti)
+);
+
+CREATE TABLE Alalumilaadut (
+  Alalumilaaduti_ID int(10) unsigned NOT NULL AUTO_INCREMENT,
+  Alatyypin_nimi varchar(45) DEFAULT NULL,
+  Lumilaadut_ID bigint(20) unsigned DEFAULT NULL,
+  Hiihdettavyys int(10) DEFAULT NULL,
+  PRIMARY KEY (Alalumilaaduti_ID),
+  KEY Lumi_alatyyppi_ibfk_1_idx(Lumilaadut_ID),
+  CONSTRAINT Alalumilaadut_ibfk_1 
+  FOREIGN KEY (Lumilaadut_ID) references Lumilaadut(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
