31638eeac72cb1fc481c4c90a4b3492c98072ec0
abdullaharif <me@example.com>
2021-10-31 21:31:02 +0200

    db update

--- src/sql/luonnit.sql
+++ src/sql/luonnit.sql
@@ -20,8 +20,7 @@ CREATE TABLE Kayttajat (
 CREATE TABLE Lumilaadut (
     ID SERIAL PRIMARY KEY,
     Nimi VARCHAR(50),
-    Vari VARCHAR(15),
-    Ennatysaika datetime DEFAULT NULL
+    Vari VARCHAR(15)
 );
 
 CREATE TABLE Paivitykset (
@@ -29,13 +28,13 @@ CREATE TABLE Paivitykset (
     Segmentti BIGINT UNSIGNED,
     Aika DATETIME,
     Lumilaatu INT,
-    Teksti TEXT,
     Kuvaus TEXT,
-    lumen_kuva BLOB,
-    Lumilaadut_ID bigint(20) unsigned,
+    Lumen_kuva BLOB,
+    Lumilaatu_ID bigint(20) unsigned,
+    Alalumilaatu_ID int(10) unsigned,
     FOREIGN KEY(Tekija) references Kayttajat(ID) ON DELETE CASCADE,
     FOREIGN KEY(Segmentti) references Segmentit(ID) ON DELETE CASCADE,
-    CONSTRAINT Lumilaadut_ID FOREIGN KEY (Lumilaadut_ID) REFERENCES Lumilaadut (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT Lumilaatu_ID FOREIGN KEY (Lumilaatu_ID) REFERENCES Lumilaatu (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
     CONSTRAINT tunniste PRIMARY KEY (Aika, Segmentti)
 );
 
@@ -48,12 +47,12 @@ CREATE TABLE Koordinaatit(
 );
 
 CREATE TABLE Alalumilaadut (
-  Alalumilaaduti_ID int(10) unsigned NOT NULL AUTO_INCREMENT,
+  Alalumilaatu_ID int(10) unsigned NOT NULL AUTO_INCREMENT,
   Alatyypin_nimi varchar(45) DEFAULT NULL,
-  Lumilaadut_ID bigint(20) unsigned DEFAULT NULL,
+  Lumilaatu_ID bigint(20) unsigned DEFAULT NULL,
   Hiihdettavyys int(10) DEFAULT NULL,
-  PRIMARY KEY (Alalumilaaduti_ID),
-  KEY Lumi_alatyyppi_ibfk_1_idx(Lumilaadut_ID),
+  PRIMARY KEY (Alalumilaatu_ID),
+  KEY Lumi_alatyyppi_ibfk_1_idx(Lumilaatu_ID),
   CONSTRAINT Alalumilaadut_ibfk_1 
-  FOREIGN KEY (Lumilaadut_ID) references Lumilaadut(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (Lumilaatu_ID) references Lumilaadut(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
